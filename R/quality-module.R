3b3168b91a8336ce2544124ffa7eb7c6a985be9e
Corrado <corrado.lanera@gmail.com>
2020-03-08 16:14:57 +0100

    Completed records (task #5)

--- R/quality-module.R
+++ R/quality-module.R
@@ -9,8 +9,8 @@
 #' @importFrom shiny fluidRow selectInput textOutput plotOutput
 #' @importFrom shiny renderText renderPlot
 #' @importFrom plotly renderPlotly plotlyOutput ggplotly
-#' @importFrom ggplot2 aes element_text ggplot xlab ylab coord_flip
-#' @importFrom ggplot2 geom_bar facet_wrap theme
+#' @importFrom ggplot2 ggplot aes geom_bar facet_wrap coord_flip
+#' @importFrom ggplot2 theme ggtitle xlab ylab element_text
 #'
 #' @name qualityReport
 NULL
@@ -26,6 +26,7 @@ qualityReportUI <- function(id) {
   fluidRow(
     textOutput(ns("stat_global")),
     textOutput(ns("out_of_age")),
+    selectInput(ns("completed"), "Records to show", c("Completed", "Not completed")),
     selectInput(ns("type"), "Summary type", c("Total", "Proportion")),
     plotOutput(ns("plot_global")),
     plotlyOutput(ns("plot_center"))
@@ -39,17 +40,21 @@ qualityReportUI <- function(id) {
 #' @export
 qualityReport <- function(id, data) {
 
-  out_of_age <- dplyr::filter(data, !.data$age_upto_18)
-  used_data <- dplyr::filter(data, .data$age_upto_18) %>%
-    dplyr::mutate(
-      name = "TIPNet",
-      complete_int = as.integer(.data$complete)
-    )
+  are_out_age <- data[["age_class"]] %in% c("[wrong/missing age]", "eta > 18")
 
   callModule(id = id, function(input, output, session) {
 
     type <- reactive({
       req(input$type)
+    })
+
+    data_to_use <- reactive({
+      req(input$completed)
+      if (input$completed == "Completed") {
+        data
+      } else {
+        dplyr::mutate(data, complete = !.data$complete)
+      }
     })
 
     fun_y <- reactive({
@@ -60,52 +65,56 @@ qualityReport <- function(id, data) {
     })
 
     output$out_of_age <- renderText(glue::glue(
-      "There are {nrow(out_of_age)} people with more than 18 years, i.e. {ui_value(out_of_age[['years']])}"
+      "There are {sum(are_out_age)} people to exclude because of their age:
+      { data_to_use()$codpat[are_out_age] %>%
+          paste(
+            paste0('(', data_to_use()$codpat[are_out_age], ')'),
+            collapse = ', '
+          )
+      }"
     ))
 
     output$stat_global <- renderText(glue::glue(
-      "There where {sum(used_data[['complete']], na.rm = TRUE)} completed data recorded (out of {nrow(used_data)}; proportion of complete data = {round(mean(used_data[['complete']], na.rm = TRUE), 2)})."
+      "Out of {sum(!are_out_age)}, there are {sum(data_to_use()[['complete']][!are_out_age], na.rm = TRUE)} records with completed data
+      (proportion of complete data = {round(mean(data_to_use()[['complete']][!are_out_age], na.rm = TRUE), 2)})."
     ))
 
-
-
-
     output$plot_global <- renderPlot({
-      used_data %>%
-        ggplot(
-          aes(x = .data$name, y = .data$complete_int)
-        ) +
+      data_to_use() %>%
+        ggplot(aes(x = "TIPNet", y = as.integer(.data$complete))) +
         geom_bar(
           stat = "summary",
-          fun.y = fun_y(), na.rm = TRUE,
+          fun.y = fun_y(),
+          na.rm = TRUE,
           position = "dodge",
-          fill = "green"
+          fill = "darkgreen"
         ) +
-        facet_wrap(~age_upto_16) +
+        facet_wrap(~.data$age_class) +
         theme(legend.position = "none") +
         xlab("") +
-        ylab("")
+        ylab("") +
+        ggtitle(paste(input$completed, "cases."))
     })
 
-
-
-
     output$plot_center <- renderPlotly({
-      gg <- used_data %>%
-        ggplot(
-          aes(x = .data$center, y = .data$complete_int, fill = .data$center)
-        ) +
+      gg <- data_to_use() %>%
+        ggplot(aes(
+          x = .data$center,
+          y = as.integer(.data$complete),
+          fill = .data$center
+        )) +
         geom_bar(stat = "summary",
             fun.y = fun_y(), na.rm = TRUE,
             position = "dodge"
         ) +
-        facet_wrap(~age_upto_16) +
+        facet_wrap(~.data$age_class) +
         theme(
           axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
           legend.position = "none"
         ) +
         xlab("") +
         ylab("") +
+        ggtitle(paste(input$completed, "cases.")) +
         coord_flip()
 
         ggplotly(gg)
