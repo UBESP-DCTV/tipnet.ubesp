1992e0d8e1996635ea30a5a25f041e5a9262cdc3
Corrado <corrado.lanera@gmail.com>
2020-04-23 16:32:06 +0200

    update default data path

--- R/db_update_from_server.R
+++ R/db_update_from_server.R
@@ -12,36 +12,37 @@
 #' \dontrun{
 #'   db_update_from_server()
 #' }
-db_update_from_server <- function(path_data = data_path(),
+db_update_from_server <- function(path_data = NULL,
                                   file_name = "tipnet.rds"
 ) {
+
+  path_data <- path_data %||% data_path()
 
   file_path <- file.path(path_data, file_name)
 
   tipnet_raw <- read_redcap(tipnet_redcap_url())
   all_ok <- tipnet_raw$data$success && tipnet_raw$meta_data$success
 
-  if (all_ok) {
-    tipnet_meta <- tidy_extract(tipnet_raw, "meta")
-
-    tipnet <- tipnet_raw %>%
-      tidy_extract("data") %>%
-      nest_tables() %>%
-      dplyr::mutate(
-        tables = purrr::map2(
-          .data$sheet, .data$tables, factorize_sheet,
-          meta = tipnet_meta
-        )
-      )
-
-    readr::write_rds(tipnet, path = file_path)
-
-    return(invisible(file_path))
-
-  } else {
+  if (!all_ok) {
     ui_warn("Connection error to REDCap (in server-start chunk).")
     return(invisible(file_path))
   }
+
+  tipnet <- tipnet_raw %>%
+    tidy_extract("data") %>%
+    nest_tables() %>%
+    dplyr::mutate(
+      tables = purrr::map2(
+        .x = .data$sheet,
+        .y = .data$tables,
+        .f = factorize_sheet,
+        meta = tidy_extract(tipnet_raw, "meta")
+      )
+    )
+
+  readr::write_rds(tipnet, path = file_path)
+
+  invisible(file_path)
 
 }
 
